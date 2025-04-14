c7ccc613f6ca9a091f74e0e9e9404329dd25e4c3
Felipe Ribeiro <f-mtz>
2025-04-14 14:20:49 -0300

    inserindo um endpoint para analise de sentimentos em frases

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 
 - `Python` 3.12.3
 - `Flask` 3.1.0
-- `Werkzeug` 3.1.3
+- `werkzeug` 3.1.3
+- `googletrans` 3.1.0a0
+
+
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from flask import Flask
+from textblob import TextBlob
+from googletrans import Translator
 
 # cria uma instancia da aplicacao flask
-app = Flask('meu_app')
+app = Flask(__name__)
 
 # definindo a rota raiz
 @app.route('/')
 def home():
     return "Minha primeira API."
 
+
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    tradutor = Translator()
+    traducao = tradutor.translate(frase, src='pt', dest='en')
+    print(traducao)
+    frase_english = traducao.text
+    tb_en = TextBlob(frase_english)
+    polaridade = tb_en.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
 # executa a API localhost na orta padrão 5000
-app.run()
+app.run(debug=True)
 
