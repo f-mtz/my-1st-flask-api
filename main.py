d825d8aa8afb545a5b8491598656eda8799b0d90
Felipe Ribeiro <f-mtz>
2025-04-14 15:21:59 -0300

    adicionando rota para previsao de preco de casa em funcao do tamanho + base de dados

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from flask import Flask
 from textblob import TextBlob
 from googletrans import Translator
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+
+# carregamento dos dados
+df = pd.read_csv('casas.csv')
+
+# definindo variaveis na regressão
+colunas = ['tamanho', 'preco']
+df = df[colunas]
+
+# separando variavel dependente e independente
+X = df.drop('preco', axis = 1)
+y = df['preco']
+
+# separacao treino e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
+
+# treinamento do modelo
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
+
 
 # cria uma instancia da aplicacao flask
 app = Flask(__name__)
@@ -20,6 +43,15 @@ def sentimento(frase):
     tb_en = TextBlob(frase_english)
     polaridade = tb_en.sentiment.polarity
     return "polaridade: {}".format(polaridade)
+
+
+@app.route('/cotacao/<int:tamanho>')
+def cotacao(tamanho):
+    preco = modelo.predict([[tamanho]])
+    formatted_prediction = "R$ {:,.2f}".format(preco[0])
+
+    return str(formatted_prediction)
+
 
 # executa a API localhost na orta padrão 5000
 app.run(debug=True)
